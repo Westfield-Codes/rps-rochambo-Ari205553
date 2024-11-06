dd6f47412e7b133f4727b8730f8220d00b7fb092
Ari205553 <Ari205553@student-schoolsofwestfield.org>
2024-11-06 16:30:50 +0000

    userturn

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+main()
+function main(){
+    let uChoice = 0; 
+    let cChoice = 0;
+    if (uChoice == cChoice){
+       uchoice = userTurn();
+       cChoice = cpuTurn(); 
+        alert("We both chose" + uChoice);
 
+    }
+    else findWinner(uChoice, cChoice);
+}
+
+
+function userTurn(){
+    let choice = prompt(" enter r,p, or s");
+    if (choice != "r" && choice != "p" && choice != "s"){
+       alert("enter r,p, or s");
+       userTurn();
+    }
+    return choice;
+}
